bb32a2d476e3c91b34a498c2a09ad8a03efe95e0
Franco Buceta <francobuceta95@gmail.com>
2022-09-20 20:48:40 -0300

    carousel clientes agregado

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,5 @@
-var swiper = new Swiper('.swiper-container', {
+//Carousel Productos
+let swiper1 = new Swiper('.swiper-container1', {
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev'
@@ -6,7 +7,7 @@ var swiper = new Swiper('.swiper-container', {
 	slidesPerView: 1,
 	spaceBetween: 10,
 	pagination: {
-		el: '.swiper-pagination',
+		el: '.swiper-pagination1',
 		clickable: true,
 	},
 
@@ -25,8 +26,43 @@ var swiper = new Swiper('.swiper-container', {
 			spaceBetween: 40,
 		},
 		1240: {
-			slidesPerView: 4,
-			spaceBetween: 50,
+			slidesPerView: 3,
+			spaceBetween: 20,
 		},
 	}
 });
+
+//Carousel Nuestros Clientes 
+let swiper2 = new Swiper('.swiper-container2', {
+	navigation: {
+		nextEl: '#button-next',
+		prevEl: '#button-prev',
+	},
+	slidesPerView: 1,
+	spaceBetween: 10,
+	pagination: {
+		el: '.swiper-pagination2',
+		clickable: true,
+	},
+
+
+	breakpoints: {
+		620: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		680: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+		920: {
+			slidesPerView: 3,
+			spaceBetween: 40,
+		},
+		1240: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+	}
+});
+
