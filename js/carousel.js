f2bbc47ca60188722afed7e93d33c811157e58c8
Franco Leon <francoleon08@hotmail.com>
2022-10-19 14:38:04 -0300

    add page

---
+++ js/carousel.js
@@ -0,0 +1,72 @@
+//Carousel Productos
+let swiper1 = new Swiper('.swiper-container1', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	slidesPerView: 1,
+	spaceBetween: 10,
+	pagination: {
+		el: '.swiper-pagination1',
+		clickable: true,
+	},
+
+
+	breakpoints: {
+		620: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		680: {
+			slidesPerView: 1,
+			spaceBetween: 40,
+		},
+		920: {
+			slidesPerView: 1,
+			spaceBetween: 40,
+		},
+		1100: {
+			slidesPerView: 1,
+			spaceBetween: 40,
+		},
+		1240: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+	}
+});
+
+//Carousel Nuestros Clientes 
+let swiper2 = new Swiper('.swiper-container2', {
+	navigation: {
+		nextEl: '#button-next',
+		prevEl: '#button-prev',
+	},
+	slidesPerView: 1,
+	spaceBetween: 10,
+	pagination: {
+		el: '.swiper-pagination2',
+		clickable: true,
+	},
+
+
+	breakpoints: {
+		620: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		680: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+		920: {
+			slidesPerView: 3,
+			spaceBetween: 40,
+		},
+		1240: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+	}
+});
+
---
+++ js/navbar_select.js
@@ -0,0 +1,50 @@
+//Select de navbar
+const navbarSelect = document.querySelector(".navbar_select");
+const navbarSelectBtn = navbarSelect.querySelector(".navbar_select-btn");
+const navbarOpciones = navbarSelect.querySelector(".navbar_select-opciones");
+const navbarSpan = navbarSelectBtn.querySelector(".navbar_select-span");
+const esconderSelect = navbarSelectBtn.querySelector(".navbar_esconder-input");
+
+navbarSelectBtn.addEventListener("click",() => {
+    navbarSelect.classList.toggle("active");
+})
+
+let serviciosNavbar = ["Obras civiles", "Mármol, Quarzo y Granito", "Lavado de Pisos", 
+"Hornos y Parrillas", "Enchapes en Piedra", "Carpintería"];
+
+let url = ["obras.html", "marmol.html", "pisos.html", "hornos_parrilas.html", "enchapes.html", 
+"carpinteria.html"];
+
+//Recorro el array de servicios y url para crear los "links" dinamicamente
+function añadirServiciosNavbar() {
+    for (let i = 0; i < serviciosNavbar.length; i++) {
+        let link;
+        if (location.pathname.includes("/pages/servicios")) {
+            for (let j = 0; j < url.length; j++) {
+                link = `<a href="${url[i]}" onclick="selectServicioNavbar(this)">${serviciosNavbar[i]}
+                        </a>`;
+            }
+        } else if (location.pathname.includes("/pages/contacto") 
+        || location.pathname.includes("/pages/nosotros")) {
+            for (let j = 0; j < url.length; j++) {
+                link = `<a href="./servicios/${url[i]}" onclick="selectServicioNavbar(this)">${serviciosNavbar[i]}
+                        </a>`;
+            }
+        } else {
+            for (let j = 0; j < url.length; j++) {
+                link = `<a href="pages/servicios/${url[i]}" onclick="selectServicioNavbar(this)">${serviciosNavbar[i]}
+                </a>`;
+            }
+        }
+        navbarOpciones.insertAdjacentHTML("beforeend", link);
+    }
+}
+
+//El link seleccionado pasa a quedar en el select
+function selectServicioNavbar(link) {
+    navbarSelect.classList.remove("active");
+    navbarSelectBtn.firstElementChild.innerText = link.innerText;
+    navbarSpan.innerHTML = "Servicios";
+}
+
+añadirServiciosNavbar();
