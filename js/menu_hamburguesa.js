1119548903ba8637f261788190cc3c7c29d86a2a
Franco Leon <francoleon08@hotmail.com>
2022-10-19 14:37:39 -0300

    reordenar rama

--- js/menu_hamburguesa.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById("menu_hamburguesa");
-const navbarPages = document.getElementById("navbar_pages");
-
-
-hamburger.addEventListener("click", () => {
-    navbarPages.classList.toggle("show");
-});
--- js/navbar_scroll.js
+++
@@ -1,9 +0,0 @@
-//Navbar cambia de color cuando se hace scroll
-const navbarScroll = document.querySelector(".navbar");
-
-window.addEventListener("scroll", () => {
-    navbarScroll.classList.add("navbar_on-scroll");
-    if (window.scrollY == 0) {
-        navbarScroll.classList.remove("navbar_on-scroll");
-    }
-})
